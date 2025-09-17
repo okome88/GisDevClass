f45191a18da77b0654f579f226763b267e4ab25e
okome <69080660+okome88@users.noreply.github.com>
2025-09-17 11:07:27 +0800

    Add files via upload

---
+++ week2/main.js
@@ -0,0 +1,43 @@
+﻿//preset map view
+const view = new ol.View({
+  center: ol.proj.fromLonLat([141.3399, 43.0782]),
+  zoom: 14
+});
+
+//preset fullscreen
+const defaultControls = ol.control.defaults;
+const FullScreen = ol.control.FullScreen;
+
+//preset and control eagle eye
+const OverviewMap = ol.control.OverviewMap;
+const source = new ol.source.OSM();
+const overviewMapControl = new OverviewMap({
+  layers: [
+    new ol.layer.Tile({
+      source: source,
+    }),
+  ],
+});
+
+//preset zoom slider
+const ZoomSlider = ol.control.ZoomSlider;
+
+//preset DragRotateAndZoom
+const DragRotateAndZoom = ol.interaction.DragRotateAndZoom;
+const defaultInteractions = ol.interaction.defaults;
+
+
+
+//new ol.Map() 建立地圖
+var map = new ol.Map({
+  interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+  controls: defaultControls().extend([overviewMapControl, new FullScreen(), new ZoomSlider()]),
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: view
+});
+
